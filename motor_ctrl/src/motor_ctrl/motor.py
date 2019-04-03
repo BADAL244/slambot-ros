ed45b0f3ae0ee92d47be15123a11c4bb0c79498a
tlin4194 <chiangt1tiff@gmail.com>
2019-04-03 10:42:22 -0400

    motor pipeline functional, make all submodule python files executable

--- motor_ctrl/src/motor_ctrl/motor.py
+++ motor_ctrl/src/motor_ctrl/motor.py
@@ -1,31 +1,55 @@
 import rospy
 import time
+import atexit
 
 from geometry_msgs.msg import Twist, Pose
 from std_msgs.msg import String, Float32
 from nav_msgs.msg import Odometry
 
-from adafruit_motorkit import MotorKit
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
 class MotorController:
   def __init__(self):
     rospy.init_node("motor")
     self.nodename = rospy.get_name()
     rospy.loginfo("%s started" % self.nodename)
-    self.mk = MotorKit()
+    mh = Adafruit_MotorHAT(addr=0x60)
+    self.left = mh.getMotor(1)
+    self.right = mh.getMotor(2)
     rospy.on_shutdown(self.motor_off)
-    rospy.Subscriber("motor/left/motor_cmd", Float32, callback=self.on_left)
-    rospy.Subscriber("motor/right/motor_cmd", Float32, callback=self.on_right)
+    atexit.register(self.motor_off)
+    rospy.Subscriber("left/motor_cmd", Float32, callback=self.on_left)
+    rospy.Subscriber("right/motor_cmd", Float32, callback=self.on_right)
 
   def on_left(self, motor_cmd):
-    mk.motor1.throttle = motor_cmd
+    motor_cmd = int(motor_cmd.data)
+    print("left_cmd: %d") % (motor_cmd)
+    if motor_cmd == 0:
+      print("Release Left")
+      self.left.run(Adafruit_MotorHAT.RELEASE)
+    elif motor_cmd > 0:
+      self.left.setSpeed(motor_cmd)
+      self.left.run(Adafruit_MotorHAT.FORWARD)
+    else:
+      self.left.setSpeed(-motor_cmd)
+      self.left.run(Adafruit_MotorHAT.BACKWARD)
 
   def on_right(self, motor_cmd):
-    mk.motor2.throttle = motor_cmd
+    motor_cmd = int(motor_cmd.data)
+    print("right_cmd: %d") % (motor_cmd)
+    if motor_cmd == 0:
+      print("Release Right")
+      self.right.run(Adafruit_MotorHAT.RELEASE)
+    elif motor_cmd > 0:
+      self.right.setSpeed(motor_cmd)
+      self.right.run(Adafruit_MotorHAT.FORWARD)
+    else:
+      self.right.setSpeed(-motor_cmd)
+      self.right.run(Adafruit_MotorHAT.BACKWARD)
 
-  def motor_off():
-    mk.motor1.throttle = 0
-    mk.motor2.throttle = 0
+  def motor_off(self):
+    self.left.run(Adafruit_MotorHAT.RELEASE)
+    self.right.run(Adafruit_MotorHAT.RELEASE)
 
 def main():
   MotorController()
