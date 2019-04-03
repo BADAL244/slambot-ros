868a2d7824ca8f8785cb007938846fde2c1035df
tlin4194 <chiangt1tiff@gmail.com>
2019-04-03 08:52:12 -0400

    motor pipeline working (except for python3 adafruit motor library)

--- motor_ctrl/src/motor_ctrl/encoder.py
+++ motor_ctrl/src/motor_ctrl/encoder.py
@@ -2,50 +2,64 @@ import rospy
 from std_msgs.msg import Int16
 import RPi.GPIO as GPIO
 
-def publish_encoder():
-  L_pub = rospy.Publisher('lwheel', Int16, queue_size=10)
-  R_pub = rospy.Publisher('rwheel', Int16, queue_size=10)
-  rospy.init_node('encoder', anonymous=True)
-  rate = rospy.Rate(10) # 10hz
+class Encoder():
 
-  e1_A_pin = 22
-  e1_B_pin = 23
-  e2_A_pin = 24
-  e2_B_pin = 25
+  def __init__(self):
+    rospy.init_node('encoder')
+    self.nodename = rospy.get_name()
+    rospy.loginfo("%s started" % self.nodename)
+    rate = rospy.Rate(10) # 10hz
 
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(e1_A_pin, GPIO.IN)
-  GPIO.setup(e1_B_pin, GPIO.IN)
-  GPIO.setup(e2_A_pin, GPIO.IN)
-  GPIO.setup(e2_B_pin, GPIO.IN)
+    L_pub = rospy.Publisher('left', Int16, queue_size=20)
+    R_pub = rospy.Publisher('right', Int16, queue_size=20)
 
-  outcome = [0,-1,1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0]
+    self.e1_A_pin = 22
+    self.e1_B_pin = 23
+    self.e2_A_pin = 24
+    self.e2_B_pin = 25
+    self.resolution = 5
 
-  e1_last_AB = 0b00
-  e2_last_AB = 0b00
-  e1 = 0
-  e2 = 0
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.e1_A_pin, GPIO.IN)
+    GPIO.setup(self.e1_B_pin, GPIO.IN)
+    GPIO.setup(self.e2_A_pin, GPIO.IN)
+    GPIO.setup(self.e2_B_pin, GPIO.IN)
 
-  while not rospy.is_shutdown():
-    e1_A = GPIO.input(e1_A_pin)
-    e1_B = GPIO.input(e1_B_pin)
-    e2_A = GPIO.input(e2_A_pin)
-    e2_B = GPIO.input(e2_B_pin)
-    e1_curr_AB = (e1_A << 1) | e1_B
-    e2_curr_AB = (e2_A << 1) | e2_B
-    e1_pos = (e1_last_AB << 2) | e1_curr_AB
-    e2_pos = (e2_last_AB << 2) | e2_curr_AB
-    e1 += outcome[e1_pos]
-    e2 += outcome[e2_pos]
-    e1_last_AB = e1_curr_AB
-    e2_last_AB = e2_curr_AB
-    #print("R {} L {}".format(e1, e2))
-    L_pub.publish(e2)
-    R_pub.publish(e1)
-    rate.sleep()
+    self.outcome = [0,-1,1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0]
 
-if __name__ == '__main__':
+    self.e1_last_AB = 0b00
+    self.e2_last_AB = 0b00
+    self.e1 = 0
+    self.e2 = 0
+
+    while not rospy.is_shutdown():
+      self.read_encoder()
+      L_pub.publish(self.e2)
+      R_pub.publish(self.e1)
+      # rate.sleep()
+
+  def read_encoder(self):
+    e1_diff = 0
+    e2_diff = 0
+    while(e1_diff == 0 and e2_diff == 0):
+      e1_A = GPIO.input(self.e1_A_pin)
+      e1_B = GPIO.input(self.e1_B_pin)
+      e2_A = GPIO.input(self.e2_A_pin)
+      e2_B = GPIO.input(self.e2_B_pin)
+      e1_curr_AB = (e1_A << 1) | e1_B
+      e2_curr_AB = (e2_A << 1) | e2_B
+      e1_pos = (self.e1_last_AB << 2) | e1_curr_AB
+      e2_pos = (self.e2_last_AB << 2) | e2_curr_AB
+      e1_diff = self.outcome[e1_pos]
+      e2_diff = self.outcome[e2_pos]
+      self.e1 += e1_diff
+      self.e2 += e2_diff
+      self.e1_last_AB = e1_curr_AB
+      self.e2_last_AB = e2_curr_AB
+      #print("R {} L {}".format(e1, e2))
+
+def main():
   try:
-    publish_encoder()
+    Encoder()
   except rospy.ROSInterruptException:
     pass
