7a06857639ad5de0fb96e0f0205019f5a04216d4
kanupriyaa97 <kanupriyaa_star@yahoo.com>
2019-04-14 21:02:53 -0400

    fix

--- slam/src/icp.py
+++ slam/src/icp.py
@@ -29,6 +29,9 @@ def ICP(A,B,T):
         return msg
 
     return_scan = B
+
+    B = B.ranges
+    A = A.ranges
     #make matrices from scans
 
     src = []
@@ -89,8 +92,7 @@ def findClosestPoint(A,B,i):
 
 def subscriber_map(scan):
     global A
-    A = scan.ranges
-    rospy.loginfo("I heard scan A %s",str(A)[1:-1])
+    rospy.loginfo("I heard scan A %s",str(A.ranges)[1:-1])
 
 def subscriber_encoder(tf):
     global T
@@ -98,8 +100,7 @@ def subscriber_encoder(tf):
 
 def subscriber_rplidar(scan):
     global B
-    B = scan.ranges
-    rospy.loginfo("I heard scan B %s",str(B)[1:-1])
+    rospy.loginfo("I heard scan B %s",str(B.ranges)[1:-1])
     icp_pub.publish(ICP(A,B,None))
 
 #Subscribers
